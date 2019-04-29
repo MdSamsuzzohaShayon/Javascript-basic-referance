6d88c8c6f37a1cc52e93c9035af1d9b203df7eef
Md Shayon <mdshayon0@gmail.com>
2019-04-30 04:21:12 +0600

    window, animation, list, list of list, element, etc

---
+++ window-animation-more/main.js
@@ -0,0 +1,80 @@
+// https://developer.mozilla.org/en-US/docs/Web/API
+
+
+// document.body;
+// console.log(document.body);
+// console.log(document.URL);
+// console.log(document.all);
+// console.log(document.fonts);
+// console.log(document.scripts);
+// console.log(document.cookie);
+// https://developer.mozilla.org/en-US/docs/Web/API/Document
+
+
+
+//  WINDOW METHODS AND PROPERTIES
+
+// window.alert("Alert the window");
+// console.log(Window.fullScreen = '300px');
+// console.log(Window.history = "Don't know");
+// console.log(Window.directories);
+// window.blur();
+// let doc  = window.document;
+// console.log("this is title: "+ doc.title);
+// window.close();
+// let placeholder = window.find("Rome");
+// console.log(placeholder);
+// window.scrollTo(0, 1000);
+// https://developer.mozilla.org/en-US/docs/Web/API/Window
+
+
+
+
+
+
+
+// LIST OF LIST 
+/*
+// console.log(document.getElementsByClassName('list'));
+const list =document.getElementsByClassName('list') ;
+// console.log(list[2]);
+
+// console.log(list.item(2));
+// list[1].childNodes;
+// console.log(list[1].childNodes[1]);
+// list[1].childNodes[1].textContent = "Hello Barca";
+
+// PRINTING ONE DIMENSIONAL ARRAY
+// for(let i =0 ; i < list.length ; i++){
+//     console.log(list[i]);
+// }
+
+// PRINTING MULTI DIMENSIONAL ARRAY
+for(let i =0 ; i < list.length ; i++){
+    for(let j =0; j<list[i].childNodes.length; j++){
+        console.log(list[i].childNodes[j]);        
+    }
+}
+*/
+
+// SELECT BY TAG WITH CLASS
+// const segment = document.querySelectorAll('div.ui.four.column.grid.container');
+// console.log(segment);
+
+
+// ELEMENT PROPERTIES AND METHODS
+/*
+let att = document.getElementsByClassName('column')[0];
+console.log(att.attributes);
+console.log(att.classList);
+att.animate([
+    // keyframes
+    { transform: 'translateY(0px)' }, 
+    { transform: 'translateY(-300px)' }
+], { 
+    // timing options
+    duration: 1000,
+    iterations: Infinity
+});
+https://developer.mozilla.org/en-US/docs/Web/API/Element#Methods
+*/

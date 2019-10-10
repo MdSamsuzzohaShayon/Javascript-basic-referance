486b8b5267437c8e512ea3297f666d43ab96810b
Md Shayon <mdshayon0@gmail.com>
2019-10-11 01:15:37 +0600

    design for js animation

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS Animation 
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/Element/animate)
+
+ - [Keyframes format](https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Keyframe_Formats)
+
+ - [Properties, Events, Methods](https://developer.mozilla.org/en-US/docs/Web/API/Animation)
+
+ - [Effect Timing](https://developer.mozilla.org/en-US/docs/Web/API/EffectTiming)
+
+ - [Keyframes effect](https://developer.mozilla.org/en-US/docs/Web/API/KeyframeEffect)
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-document.getElementById("img-holder").animate([
-    // keyframes
-    { transform: 'translateX(0px)' }, 
-    { transform: 'translateX(500px)' }
-  ], { 
-    // timing options
-    duration: 1000,
-    iterations: Infinity
-  });
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+// document.getElementById("img-holder").animate([
+//     // keyframes
+//     { transform: 'translateX(0px)' }, 
+//     { transform: 'translateX(100%)' }
+//   ], { 
+//     // timing options
+//     duration: 2000,
+//     iterations: Infinity
+//   });

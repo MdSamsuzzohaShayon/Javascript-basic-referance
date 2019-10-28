0d7fdd7d088fa37cbbe79af0217a8667b3e59944
Md Shayon <mdshayon0@gmail.com>
2019-10-29 01:45:45 +0600

    Animation is working- two item animated

--- README.md
+++ README.md
@@ -9,3 +9,58 @@
  - [Effect Timing](https://developer.mozilla.org/en-US/docs/Web/API/EffectTiming)
 
  - [Keyframes effect](https://developer.mozilla.org/en-US/docs/Web/API/KeyframeEffect)
+
+
+
+ ## Problem
+
+
+ Hlw everyone, I am making a simple animation with javascript. I used Animation class from Web Animation API. I just want to create few more class want to animate different element with I can replace transform value of translateX by creating a new class. I have setup my constructor I just want to dynamically pass the value to translateX; [This is my full project ](https://codepen.io/mdsamsuzzohashayon/pen/XWWgRZg) And js code is here 
+
+ 
+ ``` 
+ // https://codepen.io/rachelnabors/pen/eJyWzm/?editors=0010 
+ const car = document.getElementById("car"); 
+ // CAR ANIMATION 
+ class CarAnimate { 
+     // car = document.getElementById("car"); 
+     constructor(tFormXStart , tFormYStart, tFormXStop, tFormYStop, startOpacity, stopOpacity) { 
+         this.tFormXStart = tFormXStart; 
+         this.tFormYStart = tFormYStart; 
+         this.tFormXStop = tFormXStop; 
+         this.tFormYStop = tFormYStop; 
+         this.startOpacity = startOpacity; 
+         this.stopOpacity = stopOpacity; 
+         } 
+         carRightKeyframes = new KeyframeEffect( 
+             car, 
+             [ 
+                 { 
+                     // WANT TO SET THE VALUE OF TRANSLATE X DYNAMICALLY
+                      //HELP IF YOU CAN 
+                    transform:'translateX(0%)', 
+                }, 
+                { 
+                    transform: 'translateX(100%)',
+                } 
+            ], 
+            { 
+                duration: 3000, 
+                fill: 'forwards', 
+                iterations: Infinity 
+            }, 
+            ); 
+            
+            carRightAnimation = new Animation(this.carRightKeyframes, document.timeline); 
+            start(){ 
+                console.log(this.carRightAnimation.getTiming); 
+                this.carRightAnimation.play(); }
+    }
+    
+     
+            const carAnimate =new CarAnimate('translateX(0%)', 'translateY(0%)', 'translateX(100%)', 'translateY(0%)',0, 1); 
+            const time = carAnimate.getTiming console.log(time); 
+            carAnimate.start(); 
+ ``` 
+
+ thank you
--- js/main.js
+++ js/main.js
@@ -15,35 +15,147 @@
 // document.body.style.overflow= "hidden";
 
 
-// const car = document.getElementById("car");
+
+
+/*
+
+class AnimationElements {
+    constructor(id, duration, iterations, tXStart, tXStop, tYStart, tYStop) {
+        this.id = id;
+        this.duration = duration;
+        this.iterations = iterations;
+        this.tXStart = tXStart;
+        this.tXStop = tXStop;
+        this.tYStart = tYStart;
+        this.tYStop = tYStop;
+
+    }
+
+    
+    animationMade = () => {
+        let element = document.getElementById(this.id);
+        element.animate([
+            {
+                transform: 'translateX('+this.tXStart+'%)'
+            },
+            {
+                transform: 'translateX('+this.tXStop+'%)'
+            }
+        ], {
+            duration: this.duration,
+            iterations: this.iterations
+        })
+    }
+}
+
+const carAnimation = new AnimationElements('car', 2000, Infinity, '0', '100', '0', '0');
+carAnimation.animationMade();
+*/
+
+
+
+
+
+// document.getElementById("img-holder").animate([
+//     // keyframes
+//     { transform: 'translateX(0px)' }, 
+//     { transform: 'translateX(100%)' }
+//   ], { 
+//     // timing options
+//     duration: 2000,
+//     iterations: Infinity
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://codepen.io/rachelnabors/pen/eJyWzm/?editors=0010
+const car = document.getElementById("car");
+const cloud_one = document.getElementById("cloud_one");
+const cloud_two = document.getElementById("cloud_two");
 
 
 
 // CAR ANIMATION
 class CarAnimate {
-    car = document.getElementById("car");
-    constructor(){
-        carRightKeyframes = new KeyframeEffect(
-            car,
+    // car = document.getElementById("car");
+    constructor(tXStart, tXStop, tYStart, tYStop, startOpacity, stopOpacity) {
+        this.tXStart = tXStart;
+        this.tXStop = tXStop;
+        this.tYStart = tYStart;
+        this.tYStop = tYStop;
+        this.startOpacity = startOpacity;
+        this.stopOpacity = stopOpacity;
+    }
+
+
+
+
+
+
+    start(id, dur, ite) {
+        let carRightKeyframes = new KeyframeEffect(
+            id,
             [{
-                    transform: 'translateX(0%)'
+                    // transform: 'translateY(' + this.tYStart + '%)',
+                    transform: 'translateX(' + this.tXStart + '%)',
+                    opacity: this.startOpacity
                 },
                 {
-                    transform: 'translateX(100%)'
+                    // transform: 'translateY(' + this.tYStop + '%)',
+                    transform: 'translateX(' + this.tXStop+ '%)',
+                    opacity: this.stopOpacity
                 }
             ], {
-                duration: 3000,
+                duration: dur,
                 fill: 'forwards',
-                iterations: Infinity
-            }
+                iterations: ite
+            },
+
         );
+
+
+
+        let carRightAnimation = new Animation(carRightKeyframes, document.timeline);
+        carRightAnimation.play(dur, ite);
     }
-    carRightAnimation = new Animation(carRightKeyframes, document.timeline);
-    start(){
-        carRightAnimation.play();
-    }
+
 }
 
+// CAR ANIMATION 
+const carAnimate = new CarAnimate('0', '100', '0', '0', 0, 1);
+carAnimate.start(car, 2000, Infinity);
 
-const CarAnimate = new CarAnimate()
-CarAnimate.start();
+
+
+// CLOUDS ANIMATION
+const cloudAnimate = new CarAnimate('0', '100', '0', '100', 0.8, 1);
+carAnimate.start(cloud_one, 10000, Infinity);
+carAnimate.start(cloud_two, 15000, Infinity);
+
+
+
+
+// const allAnimations = document.timeline.getAnimations();
+// allAnimations.forEach((ani) => {
+//   ani.pause();
+//   console.log(ani.playState);
+//   ani.reverse();
+// });

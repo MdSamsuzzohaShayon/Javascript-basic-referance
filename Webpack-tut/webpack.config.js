cb06347fbc656d33cafeb60eeb17ae7cca32b4f9
Md Shayon <mdshayon0@gmail.com>
2021-04-23 06:16:40 +0600

    updated

--- Webpack-tut/webpack.config.js
+++ Webpack-tut/webpack.config.js
@@ -5,12 +5,25 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
                 test: /\.html$/,
                 use: [
                     {
                         loader: "html-loader",
                         options: { minimize: true }
                     }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    { loader: 'file-loader' }
                 ]
             }
         ]

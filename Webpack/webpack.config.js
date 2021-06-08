08e5cdf6f8a0ccfb8413a538a5a1421b16f866a3
Md Shayon <mdshayon0@gmail.com>
2021-06-08 18:06:23 +0600

    configure webpack for css

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -9,5 +9,17 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    // https://webpack.js.org/loaders/css-loader/
+    // npm install --save-dev css-loader
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
     }
 };
+
+// 48:00

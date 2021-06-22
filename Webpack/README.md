cc9c696fbb97ea2fe8f9451ce02ebfae3066a378
Md Shayon <mdshayon0@gmail.com>
2021-06-22 08:53:58 +0600

    babel

--- Webpack/README.md
+++
@@ -1,3 +0,0 @@
-# Webpack
- - [Tutorial](https://www.youtube.com/watch?v=MpGLUVbqoYQ)
- - Run this app by using command `npm start`
--- Webpack/src/app/utils/parse-inputs.js
+++
@@ -1,5 +0,0 @@
-// UTILS ARE REUSABLE FUNCTIONS 
-
-export const parseInputs = (...input) => {
-    return input.map(str => parseInt(str));
-};
